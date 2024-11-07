a77dc6e697a052272ed57d6f3054f35ecda69748
marekzska <marekzska@gmail.com>
2024-11-07 17:17:13 +0100

    Solved build issues

--- app/components/ComingNetworkings.tsx
+++ app/components/ComingNetworkings.tsx
@@ -10,7 +10,7 @@ export const ComingNetworkings = async () => {
         <div className="w-full py-10 sm:py-16 xl:py-10">
             <div className="mx-auto max-w-screen-2xl px-6 lg:px-10 xl:px-20 grid auto-rows-fr gap-6 items-center">
                 {upcomingEvents.map((event) => (
-                    <NetworkingRecord {...event} />
+                    <NetworkingRecord key={event.slug} {...event} />
                 ))}
                 <div className="flex flex-col lg:flex-row bg-networking-green w-full h-full p-6">
                     <div className="lg:w-3/5 my-auto" dangerouslySetInnerHTML={{ __html: newsletterText.text }} />
