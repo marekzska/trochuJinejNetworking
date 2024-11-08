d9a25c813c0c3f70b45731d4ec7aec0a885bd977
marekzska <marekzska@gmail.com>
2024-11-08 19:52:35 +0100

    Solved width issue in VideoSection

--- app/components/VideoSection.tsx
+++ app/components/VideoSection.tsx
@@ -6,7 +6,7 @@ export const VideoSection = async () => {
     return (
         <div className="w-full">
             <div className="mx-auto max-w-screen-2xl px-6 lg:px-10 xl:px-20 flex flex-col sm:flex-row justify-between sm:items-center py-16 xl:py-10 gap-10 sm:gap-0">
-                <div className="sm:w-2/5 xl:w-1/3 text-3xl leading-8 lg:leading-10 xl:pr-12 text-xl lg:text-3xl" dangerouslySetInnerHTML={{ __html: videoSection.text }} />
+                <div className="sm:w-2/5 xl:w-5/12 text-3xl leading-8 lg:leading-10 xl:pr-12 text-xl lg:text-3xl" dangerouslySetInnerHTML={{ __html: videoSection.text }} />
                 <div className="sm:w-1/2 flex gap-3 items-center">
                     {/* <Image unoptimized={true} src={`${process.env.NEXT_PUBLIC_CMS_URL}${videoSection.leftImage.url}`} width={300} height={600} alt={`${process.env.NEXT_PUBLIC_CMS_URL}${videoSection.leftImage.hash}`} className="rounded-3xl w-1/4 h-auto" /> */}
                     <Image unoptimized={true} src={`/images/firstImage.png`} width={300} height={600} alt={`first image`} className="rounded-3xl w-1/4 h-auto" />
