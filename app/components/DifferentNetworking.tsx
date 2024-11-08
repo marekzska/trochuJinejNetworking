50e6e072a28f939a4af8429be2c7f55b1641ebbe
marekzska <marekzska@gmail.com>
2024-11-08 19:55:57 +0100

    Solved padding issue in DifferentNetworking

--- app/components/DifferentNetworking.tsx
+++ app/components/DifferentNetworking.tsx
@@ -5,8 +5,8 @@ export const DifferentNetworking = async () => {
     const differentNetworkingData = await getDifferentNetworkingSectionData()
     return (
         <div className="w-full py-10 sm:py-16 xl:py-10 h-full">
-            <div className="mx-auto h-full max-w-screen-2xl px-6 lg:px-10 xl:px-20 flex flex-col xl:flex-row justify-between xl:items-center gap-10 xl:gap-0">
-                <div className="xl:w-1/2 xl:pr-28 text-lg" dangerouslySetInnerHTML={{ __html: differentNetworkingData.text }} />
+            <div className="mx-auto h-full max-w-screen-2xl px-6 lg:px-10 xl:px-20 flex flex-col xl:flex-row justify-between xl:items-center gap-10 xl:gap-28">
+                <div className="xl:w-1/2 text-lg" dangerouslySetInnerHTML={{ __html: differentNetworkingData.text }} />
                 <div className="xl:w-1/2 aspect-square sm:aspect-video rounded-3xl relative overflow-hidden">
                     {/* <Image unoptimized={true} src={`${process.env.NEXT_PUBLIC_CMS_URL}${differentNetworkingData.image.url}`} alt={differentNetworkingData.image.hash} fill={true} className="object-cover" /> */}
                     <Image unoptimized={true} src={`/images/differentNetworkingImage.png`} alt={'different networking image'} fill={true} className="object-cover" />
