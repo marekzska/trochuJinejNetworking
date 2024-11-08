da80d85c2a4ba801af224df83843aea85f68c523
marekzska <marekzska@gmail.com>
2024-11-08 19:50:21 +0100

    Solving a padding issue on homepage

--- app/components/OurVision.tsx
+++ app/components/OurVision.tsx
@@ -5,12 +5,12 @@ export const OurVision = async () => {
     const visionSectionData = await getVisionSectionData();
     return (
         <div className="w-full py-10 sm:py-16 xl:py-10 h-full">
-            <div className="mx-auto h-full max-w-screen-2xl px-6 lg:px-10 xl:px-20 flex flex-col-reverse xl:flex-row justify-between items-center gap-10 xl:gap-0">
+            <div className="mx-auto h-full max-w-screen-2xl px-6 lg:px-10 xl:px-20 flex flex-col-reverse xl:flex-row justify-between items-center gap-10 xl:gap-36">
                 <div className="w-full aspect-square sm:aspect-video xl:aspect-none xl:w-1/2 xl:h-full rounded-3xl relative overflow-hidden">
                     {/* <Image unoptimized={true} src={`${process.env.NEXT_PUBLIC_CMS_URL}${visionSectionData.image.url}`} alt={visionSectionData.image.hash} fill={true} className="object-cover" /> */}
                     <Image unoptimized={true} src={'/images/NetworkingJinejObrazek.jpg'} alt={'vision image'} fill={true} className="object-cover" />
                 </div>
-                <div className="xl:w-1/2 xl:pl-36 text-lg" dangerouslySetInnerHTML={{ __html: visionSectionData.text }} />
+                <div className="xl:w-1/2 text-lg" dangerouslySetInnerHTML={{ __html: visionSectionData.text }} />
             </div>
         </div>
     )
